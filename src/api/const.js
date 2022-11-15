6ab8f7e5f59c66ae71ab544530e240dccd922c33
NekrasovaOlga <33664274+NekrasovaOlga@users.noreply.github.com>
2022-11-15 22:48:46 +0300

    fix const Redirect_URL

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'l751sT0PiP35GE6Bm0cstA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'RANDOM_STRING';
-export const REDIRECT = 'http://localhost:3000/auth';
+export const REDIRECT = 'https://blogget-pi.vercel.app/auth';
 export const SCOPE_STRING = 'identity submit read';
